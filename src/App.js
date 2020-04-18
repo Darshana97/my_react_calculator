995229b06f162771f928356accda8ccf21e92cee
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2020-04-18 10:22:57 +0530

    buttons

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Buttons from "./components/Button";
+import Output from "./components/Output";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+  state = {
+    result: "",
+  };
+
+  buttonPressed = (buttonName) => {
+    if (buttonName === "=") {
+      this.calculate();
+    } else
+      this.setState({
+        result: this.state.result + buttonName,
+      });
+  };
+
+  calculate = () => {
+    this.setState({
+      result: eval(this.state.result),
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Output result={this.state.result} />
+        <Buttons buttonPressed={this.buttonPressed} />
+      </div>
+    );
+  }
 }
 
 export default App;
